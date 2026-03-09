39430b2023b4910158926e5abf582fe2954e99fd
Luccas Jesus <luccasjesus01011001@gmail.com>
2026-03-08 21:34:28 -0300

    Nova atualização das views e back-end 08/03

--- resources/views/tarefa.blade.php
+++ resources/views/tarefa.blade.php
@@ -3,15 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{url('css/tarefa.css')}}">
     <title>View Tarefa</title>
 </head>
 <body>
-    
+    <div class="tarefas">
     @foreach($tarefa as $c)
         <p>{{$c->id}}</p>
-        <p>{{$c->name}}</p>
-        <p>{{$c->email}}</p>
+        <h1>{{$c->tituloTarefa}}</h1>
+        <p>{{$c->descTarefa}}</p>
+        <p>{{$c->prazo}}</p>
+        <p>{{$c->prioridade}}</p>
+        <p>{{$c->status}}</p>
     @endforeach
-
+    </div>
 </body>
 </html>
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -3,15 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>View Tarefa</title>
+    <link rel="stylesheet" href="{{url('css/users.css')}}">
+    <title>View Users</title>
 </head>
 <body>
-    
+    <nav class="navbar">
+        <h1>Usuarios!</h1>
+    </nav>
+    <div class="conteudo">
     @foreach($users as $c)
-        <p>{{$c->id}}</p>
-        <p>{{$c->name}}</p>
-        <p>{{$c->email}}</p>
+    <div class="usuarios">
+        <p>ID: {{$c->id}}</p>
+        <p> NOME: {{$c->name}}</p>
+        <p>EMAIL: {{$c->email}}</p>
+    </div>
     @endforeach
-
+    </div>
 </body>
 </html>
